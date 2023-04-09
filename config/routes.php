0c05f195ffddbfe7e102296a0e8ff268db5108ce
4ndrisw <4ndr1sw@gmail.com>
2023-04-09 16:56:47 +0700

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -11,4 +11,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 //$route['statements/show/(:num)/(:any)'] = 'mystatement/show/$1/$2';
 //$route['statements/pdf/(:num)'] = 'myschedule/pdf/$1';
 
-$route['statements/payment/(:num)'] = 'statements/remittances/payment/$1';
+$route['statements/myremittances']               = 'Mystatement/index'; //OK
+
+$route['statements/payment/(:num)'] = 'remittances/payment/$1';
+$route['statements/remittance/pdf/(:num)'] = 'remittances/pdf/$1';
